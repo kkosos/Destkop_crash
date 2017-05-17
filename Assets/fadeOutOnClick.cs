d6ae3c4336d18a09bf22af40bf7e021845a0fc30
aaaaagold <alexhhxela@gmail.com>
2017-05-17 18:19:47 +0800

    Update fadeOutOnClick.cs
    GG

--- Assets/fadeOutOnClick.cs
+++ Assets/fadeOutOnClick.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 public class fadeOutOnClick : MonoBehaviour {
-    private Texture2D txt2d;
+    public Texture2D txt2d=null;
 	public string toScene="";
     public int frames = 111;
     bool click = false;
@@ -10,14 +10,20 @@ public class fadeOutOnClick : MonoBehaviour {
     float alpha=0;
     void Start()
     {
+		if(txt2d==null) return;
         txt2d = new Texture2D(Screen.width, Screen.height);
         for (int y = 0; y < Screen.height; y++) for (int x = 0; x < Screen.width; x++) txt2d.SetPixel(x, y, Color.black);
         txt2d.Apply();
+		//txt2d.transform.parent=GameObject.Find("screen").transform;
     }
     // Use this for initialization
     public void Click()
     {
-        click = true;
+		//SteamVR_Fade.View(Color.white,0.0f);
+		Color t=Color.black;
+		t.a=1f;
+        SteamVR_Fade.View(t,5.0f);
+        //click = true;
         Debug.Log("AAA");
         //StartCoroutine(changeScene());
     }
@@ -33,6 +39,7 @@ public class fadeOutOnClick : MonoBehaviour {
             Color c = GUI.color;
             c.a = alpha = Mathf.Clamp01(alpha);
             GUI.color = c;
+			//GUI.depth=-1000;
             GUI.DrawTexture(new Rect(0, 0, Screen.width, Screen.height), txt2d);
             if (alpha >= 1) change = true;
             else alpha += 1.0f/frames;
